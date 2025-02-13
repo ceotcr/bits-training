be4b96575982d151916987976d08ccaecff54d17
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2025-02-13 10:49:53 +0530

    Assignment 7.1 EMS with TS

--- Assignment7/EMS.js
+++ Assignment7/EMS.js
@@ -33,3 +33,82 @@ class GenericStorage {
 function updateSalary(employee, newSalary) {
     return { ...employee, salary: newSalary };
 }
+
+const emp101 = { id: 101, name: 'Chetan', position: 'Developer', salary: 50000 };
+const emp102 = { id: 102, name: 'TCR', position: 'Project Manager', salary: 50000, teamSize: 5 };
+const emp103 = { id: 103, name: 'Chetan Sapkal', position: 'Tester', salary: 50000 };
+
+const dept = new Department();
+
+console.log('\nAdding employees to department');
+dept.addEmployee(emp101);
+dept.addEmployee(emp102);
+dept.addEmployee(emp103);
+dept.listEmployees();
+
+console.log('\nRemoving employee from department');
+dept.removeEmployee(101);
+dept.listEmployees();
+
+console.log('\nTotal salary of department:', dept.getTotalSalary());
+
+const storage = new GenericStorage();
+storage.addData(emp101);
+storage.addData(emp102);
+storage.addData(emp103);
+console.log('\nData in storage:', storage.getData());
+
+console.log('\nUpdating salary of employee');
+
+const updatedEmp = updateSalary(emp101, 60000);
+console.log('Updated employee:', updatedEmp);
+console.log('Original employee:', emp101);
+
+
+/* Output:
+node .\EMS.js
+
+Adding employees to department
+[
+  { id: 101, name: 'Chetan', position: 'Developer', salary: 50000 },
+  {
+    id: 102,
+    name: 'TCR',
+    position: 'Project Manager',
+    salary: 50000,
+    teamSize: 5
+  },
+  { id: 103, name: 'Chetan Sapkal', position: 'Tester', salary: 50000 }
+]
+
+Removing employee from department
+[
+  {
+    id: 102,
+    name: 'TCR',
+    position: 'Project Manager',
+    salary: 50000,
+    teamSize: 5
+  },
+  { id: 103, name: 'Chetan Sapkal', position: 'Tester', salary: 50000 }
+]
+
+Total salary of department: 100000
+
+Data in storage: [
+  { id: 101, name: 'Chetan', position: 'Developer', salary: 50000 },
+  {
+    id: 102,
+    name: 'TCR',
+    position: 'Project Manager',
+    salary: 50000,
+    teamSize: 5
+  },
+  { id: 103, name: 'Chetan Sapkal', position: 'Tester', salary: 50000 }
+]
+
+Updating salary of employee
+Updated employee: { id: 101, name: 'Chetan', position: 'Developer', salary: 60000 }
+Original employee: { id: 101, name: 'Chetan', position: 'Developer', salary: 50000 }
+
+*/
--- Assignment7/EMS.ts
+++ Assignment7/EMS.ts
@@ -42,3 +42,31 @@ function updateSalary<T extends IEmployee>(employee: T, newSalary: number): T {
     return { ...employee, salary: newSalary } as T;
 }
 
+const emp101: IEmployee = { id: 101, name: 'Chetan', position: 'Developer', salary: 50000 };
+const emp102: IManager = { id: 102, name: 'TCR', position: 'Project Manager', salary: 50000, teamSize: 5 };
+const emp103: IEmployee = { id: 103, name: 'Chetan Sapkal', position: 'Tester', salary: 50000 };
+
+const dept = new Department();
+
+console.log('\nAdding employees to department');
+dept.addEmployee(emp101);
+dept.addEmployee(emp102);
+dept.addEmployee(emp103);
+dept.listEmployees();
+
+console.log('\nRemoving employee from department');
+dept.removeEmployee(101);
+dept.listEmployees();
+
+console.log('\nTotal salary of department:', dept.getTotalSalary());
+
+const storage = new GenericStorage<IEmployee>();
+storage.addData(emp101);
+storage.addData(emp102);
+storage.addData(emp103);
+
+console.log('\nData in storage:', storage.getData());
+
+console.log('\nUpdating salary of employee');
+const updatedEmp = updateSalary(emp101, 60000);
+console.log('Updated employee:', updatedEmp);
